e81a48d90eabb9c4d1acf1bf430e726043e03d30
JupiterPi <jupiterpi@outlook.de>
2023-04-17 17:27:53 +0200

    viewing files

--- src/app/ui/file-viewer/file-viewer.component.ts
+++ src/app/ui/file-viewer/file-viewer.component.ts
@@ -1,8 +1,38 @@
 import {Component} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
+import {CacheMap} from "../../../util";
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Component({
   selector: 'app-file-viewer',
   templateUrl: './file-viewer.component.html',
   styleUrls: ['./file-viewer.component.scss']
 })
-export class FileViewerComponent {}
+export class FileViewerComponent {
+  filename?: string;
+  empty = false;
+  files: string[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private storage: AngularFireStorage,
+  ) {
+    this.route.params.subscribe(params => {
+      this.filename = params["file"];
+
+      this.storage.ref("/files/" + this.filename).listAll().subscribe(data => {
+        this.files = data.items.map(item => item.name);
+        this.empty = this.files.length == 0;
+      });
+    });
+  }
+
+  downloadUrls = new CacheMap<string, Observable<string>>(file => {
+    const s = new BehaviorSubject("");
+    this.storage.ref("/files/" + this.filename + "/" + file).getDownloadURL().subscribe(url => {
+      s.next(url);
+    });
+    return s;
+  });
+}
