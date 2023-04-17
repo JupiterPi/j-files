06d2f0d43172ac77150ae36803cd91dfc44ee87a
JupiterPi <jupiterpi@outlook.de>
2023-04-17 20:43:26 +0200

    upload files

--- src/app/ui/file-viewer/file-viewer.component.ts
+++ src/app/ui/file-viewer/file-viewer.component.ts
@@ -23,15 +23,18 @@ export class FileViewerComponent {
     this.route.params.subscribe(params => {
       this.filename = params["file"];
 
-      this.storage.ref("/files/" + this.filename).listAll().subscribe(data => {
-        this.files = data.items.map(item => item.name);
-        this.empty = this.files.length == 0;
-      });
+      this.loadFiles();
 
       this.user.getUser().subscribe(user => {
         this.hasWriteAccess = user.hasWriteAccess;
         this.bookmarked = user.bookmarkedFiles.includes(this.filename!);
       });
+    });
+  }
+  private loadFiles() {
+    this.storage.ref("/files/" + this.filename).listAll().subscribe(data => {
+      this.files = data.items.map(item => item.name);
+      this.empty = this.files.length == 0;
     });
   }
 
@@ -43,5 +46,16 @@ export class FileViewerComponent {
     this.storage.ref("/files/" + this.filename + "/" + file).getDownloadURL().subscribe(url => {
       window.open(url);
     });
+  }
+
+  uploadFiles(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const fileList = target.files!;
+    for (let i = 0; i < fileList.length; i++) {
+      const file = fileList.item(i)!;
+      const task = this.storage.upload("/files/" + this.filename + "/" + file.name, file);
+      task.percentageChanges().subscribe(percentage => console.log(percentage));
+      task.then(() => this.loadFiles());
+    }
   }
 }
